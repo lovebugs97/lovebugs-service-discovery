f6a07328b72de479c8cb5c6b496df47418e2b01b
rivercity310 <h970126@gmail.com>
2024-07-16 15:00:45 +0900

    feat: heartbeat 설정 추가

--- src/main/kotlin/com/lovebugs/service_discovery/ServiceDiscoveryApplication.kt
+++ src/main/kotlin/com/lovebugs/service_discovery/ServiceDiscoveryApplication.kt
@@ -1,12 +1,19 @@
 package com.lovebugs.service_discovery
 
+import com.netflix.discovery.EurekaClient
+import jakarta.annotation.PreDestroy
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer
 
 @SpringBootApplication
 @EnableEurekaServer
-class ServiceDiscoveryApplication
+class ServiceDiscoveryApplication(private val eurekaClient: EurekaClient) {
+	@PreDestroy
+	fun unregister() {
+		eurekaClient.shutdown()
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<ServiceDiscoveryApplication>(*args)
